801e099f2e6e8778b36c57fed783740484fd0e1c
liamatkisson13-lgtm <liamatkisson13@gmail.com>
2025-08-17 14:23:05 -0400

    ligma.js
    yes, it is ligma

--- plutonium.js
+++ plutonium.js
@@ -1,8 +1,7 @@
-addIngredient("plutonium", {
-        shape:"rock",
-        color:["#16386e","#311d57"],
-        glow:"#00ff00",
-        adj:"radioactive",
-        hidden:true
-    }
-);
+
+addIngredient("Deez", {
+        shape:"powder" ,
+        color:["#FFFFFF"],
+        adj:"Nutted in",
+        hidden:false
+}
